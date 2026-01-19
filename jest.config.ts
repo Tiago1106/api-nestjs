ef425613f5a6b41dde9e3b379f7f0f7fa3eefab0
Tiago Pinheiro <tiagofreitasp00@gmail.com>
2026-01-18 23:27:48 -0300

    chore(API-04): configure sonar

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  testEnvironment: 'node',
+};
+
+export default config;
